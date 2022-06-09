bc2f971245391f59522cc83bc4ce6ea002b4edef
Pushpendra amoliya <pushpendraamoliya16@gmail.com>
2022-06-09 17:08:45 +0530

    Boredom

---
+++ Q5.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define int long long
+#define PI 3.1415926535897932384626
+#define N 1e5+1
+#define rep(j,i, n) for(int j=i;j<n;j++)
+#define Fo(i, k, n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, x) for(auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for(auto it = x.rbegin(); it != x.rend(); it+)
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vpii;
+typedef vector<vi> vvi;
+const int mod=1e9+7;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+//void _print(lli t) {cerr << t; }
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.F); cerr << ","; _print(p.S); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+void Substring(string s) { int n=s.length();for(int i=0;i<n;i++){for(int j=i;j<n;j++){for(int k=i;k<=j;k++){cout<<s[k]<<" ";}cout<<endl;}}}
+int binpow(int b,int p){int r=1;while(p){if(p&1)r=(r*b)%mod;b=(b*b)%mod;p/=2;}return r;}
+
+
+signed  main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("Error.txt","w",stderr);
+    #endif
+
+
+        int n;
+        cin>>n;
+         vi v(n);
+         rep(i,0,n) cin>>v[i];
+
+        map<int,int> m;
+        rep(i,0,n) m[v[i]]++;
+        vector<int> dp(N);
+
+         dp[0]=0;
+         dp[1]=m[1];
+        rep(i,2,N)
+        {
+            dp[i]=max(dp[i-2] + m[i]*i,dp[i-1]);
+        }
+        debug(dp);
+        cout<<dp[(int)N-1]<<endl;
+}
